4d210e6ece298ce6661bdde28f719201fd2ee7c9
ManikantaDatti <manikantadatti11@gmail.com>
2025-02-04 11:19:52 +0530

    commiting 13 file

---
+++ 14-colors.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m" 
+userid=$(id -u)
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2... $R failed $N"
+        exit 1
+    else
+        echo -e "$2... $G success $N"
+
+}
+if [ $userid -ne 0 ]
+then
+    echo "ERROR: you do not have access"
+    exit 1
+fi 
+dnf list installed mysql
+if [ $? -ne 0 ]
+then 
+    dnf install mysql -y
+    VALIDATE $? "Installing mysql" 
+else
+    echo -e "mysql is already... $Y Installed"
+fi 
+dnf list installed git 
+if [ $? -ne 0 ]
+then 
+    dnf install git -y
+    VALIDATE $? "installing git"
+else
+    echo -e "git is already... $Y installed"
+fi
