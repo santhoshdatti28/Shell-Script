37cad286b594e73f0171f9b16b83fedb50861eb8
ManikantaDatti <manikantadatti11@gmail.com>
2025-02-04 11:01:34 +0530

    commiting 13 file

---
+++ 13-functions.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+userid=$(id -u)
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo "$2...failure"
+        exit 1
+    else 
+        echo "$2...success"
+}
+if [ $userid -ne 0 ]
+then
+    echo "ERROR : You do not have access"
+    exit 1
+fi
+dnf list installed mysql
+if [ $? -ne 0 ]
+then 
+    dnf install mysql -y
+    VALIDATE $? "installing mysql"
+else 
+    echo "mysql is already installed"
+fi
+dnf list installed git
+if [ $? -ne 0 ]
+then 
+    dnf install git -y 
+    VALIDATE $? "installing git"
+else
+    echo "git is already installed"
+fi
