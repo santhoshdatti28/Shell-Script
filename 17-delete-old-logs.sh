48a59c8cbf0d018a6d30f3681e0dc1f3d2194a34
ManikantaDatti <manikantadatti11@gmail.com>
2025-02-06 15:14:06 +0530

    deleting files commiting

---
+++ 17-delete-old-logs.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+logs_folder="/var/log/expense-shell"
+log_file=$(echo $0 | cut -d "." -f1)
+timestamp=$(date +%Y-%m-%d-%H-M-%S)
+log_file_name="$logs_folder/$log_file-$timestamp.log"
+
+userid=$(id -u)
+
+CHECK_ROOT(){
+    if [ $userid -ne 0 ]
+    then
+        echo "ERROR: you do not have access"
+        exit 1
+    fi
+}
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2...$R failure $N"
+    else
+        echo -e "$2...$G success $N"
+    fi
+}
+
+CHECK_ROOT
+
+mkdir -p /var/logs/expense-shell
+
+files_delete=$(find /root/app-log "*.log" -mtime +14)
+
+while read -r $file
+do
+    echo "deleting file: $file"
+    rm -rf $file
+done <<<$files_delete
+
+
