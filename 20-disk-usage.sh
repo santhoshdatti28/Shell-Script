c3582ea157e8d55fbc6e5b30b59c6b287525d959
ManikantaDatti <manikantadatti11@gmail.com>
2025-02-15 20:13:28 +0530

    commiting 19th file

--- 20-disk-usage.sh
+++ 20-disk-usage.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 disk_usage=$(df -hT)
 disk_threshold=5
+msg=""
 
 while read -r line
 do
     #echo $line
     usage=$(echo $line | awk -F " " '{print $6F}'| cut -d "%" -f1)
     partition=$(echo $line | awk -F " " '{print $1F}')
-    echo "$partition"
+    #echo "$partition"
+    if [ $usage -ge $disk_threshold ]
+    then
+        msg="high disk usage on partition: $partition , usage is: $usage"
 
 
 done <<< $disk_usage
