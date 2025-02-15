33f8e70388b6397da3aaa2e39bc9c10b37908f37
ManikantaDatti <manikantadatti11@gmail.com>
2025-02-15 20:11:09 +0530

    commiting 19th file

--- 20-disk-usage.sh
+++ 20-disk-usage.sh
@@ -6,7 +6,7 @@ while read -r line
 do
     #echo $line
     usage=$(echo $line | awk -F " " '{print $6F}'| cut -d "%" -f1)
-    partition=$(echo $line | awk -F '{print $1F}')
+    partition=$(echo $line | awk -F " " '{print $1F}')
     echo "$partition"
 
 
