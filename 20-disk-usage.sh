f281a91f415bcbaecb61954b7b614c227a0c4955
ManikantaDatti <manikantadatti11@gmail.com>
2025-02-15 20:23:50 +0530

    commiting 19th file

--- 20-disk-usage.sh
+++ 20-disk-usage.sh
@@ -11,7 +11,7 @@ do
     #echo "$partition"
     if [ $usage -ge $disk_threshold ]
     then
-        msg+="high disk usage on partition: $partition , usage is: $usage <br>"
+        msg+="high disk usage on partition: $partition , usage is: $usage \n "
         #echo "$msg"
     fi
 
